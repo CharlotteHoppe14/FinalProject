15f64691a1fb954e12227b4c4df541a0668a9a7d
CharlotteHoppe14 <Charlotte.A.Hoppe@stud.leuphana.de>
2025-07-07 21:34:09 +0200

    Update ConcertBuddy.py

--- ConcertBuddy.py
+++ ConcertBuddy.py
@@ -1,1 +1,12 @@
-
+def create_profile():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    gender = input("Your gender (m/f/d)? ")
+    interests = input("Choose 3 interests, split with comma: ").split(",")
+    concert = input("Which concert do you visit? ")
+    return {
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "interests": interests,
+        "concert": concert
